fcc5940f898da2421de20ec589638037d6a02053
cyanray <a484599279@hotmail.com>
2021-02-02 16:09:56 +0800

    为CpdailyCore.cs添加注释;

--- cpdaily-auto-submit/CpdailyCore.cs
+++ cpdaily-auto-submit/CpdailyCore.cs
@@ -12,11 +12,14 @@ using System.Threading.Tasks;
 
 namespace cpdaily_auto_submit
 {
+    /// <summary>
+    /// Cpdaily API
+    /// </summary>
     public class CpdailyCore
     {
-        public const string ApiUserAgent = "Mozilla/5.0 (Linux; Android 5.1.1; vmos Build/LMY48G; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36 okhttp/3.12.4";
-        public const string WebUserAgent = "Mozilla/5.0 (Linux; Android 5.1.1; vmos Build/LMY48G; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36  cpdaily/8.2.16 wisedu/8.2.16";
-        public const string SaltForGetSecretKey = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
+        /// <summary>
+        /// 设备信息
+        /// </summary>
         public DeviceInfo DeviceInfo { get; set; } = new DeviceInfo()
         {
             AppVersion = "8.2.16",
@@ -28,8 +31,17 @@ namespace cpdaily_auto_submit
             Latitude = 0,
             UserId = ""
         };
+
+        private const string ApiUserAgent = "Mozilla/5.0 (Linux; Android 5.1.1; vmos Build/LMY48G; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36 okhttp/3.12.4";
+        private const string WebUserAgent = "Mozilla/5.0 (Linux; Android 5.1.1; vmos Build/LMY48G; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36  cpdaily/8.2.16 wisedu/8.2.16";
+        private const string SaltForGetSecretKey = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
         private readonly CookieContainer CookieContainer = new CookieContainer();
 
+        /// <summary>
+        /// 获取后续加密用的密钥
+        /// </summary>
+        /// <param name="guid">随机的GUID</param>
+        /// <returns>SecretKey</returns>
         public async Task<SecretKey> GetSecretKeyAsync(Guid guid)
         {
             string p = $"{guid}|firstv";
@@ -62,11 +74,19 @@ namespace cpdaily_auto_submit
             return new SecretKey { Guid = Guid.Parse(data[0]), Chk = data[1], Fhk = data[2] };
         }
 
+        /// <summary>
+        /// 获取后续加密用的密钥
+        /// </summary>
+        /// <returns>SecretKey</returns>
         public Task<SecretKey> GetSecretKeyAsync()
         {
             return GetSecretKeyAsync(Guid.NewGuid());
         }
 
+        /// <summary>
+        /// 获取所有入驻的学校
+        /// </summary>
+        /// <returns>School[]</returns>
         public async Task<School[]> GetSchoolsAsync()
         {
             string url = $"https://static.campushoy.com/apicache/tenantListSort?v={DateTimeOffset.Now.ToUnixTimeMilliseconds()}&oick={CpdailyCrypto.GetOick()}";
@@ -99,6 +119,12 @@ namespace cpdaily_auto_submit
             return schools.ToArray();
         }
 
+        /// <summary>
+        /// 获取学校的详细信息(用于登录或其他)
+        /// </summary>
+        /// <param name="schoolId">学校ID</param>
+        /// <param name="chk">通过GetSecretKeyAsync()获取到的Chk密钥</param>
+        /// <returns>SchoolDetails</returns>
         public async Task<SchoolDetails> GetSchoolDetailsAsync(string schoolId, string chk)
         {
             var encryptedId = CpdailyCrypto.DESEncrypt(schoolId, CpdailyCrypto.GetDESKey(chk), CpdailyCrypto.IV);
@@ -129,6 +155,12 @@ namespace cpdaily_auto_submit
             return JArray.Parse(decretedData)[0].ToObject<SchoolDetails>();
         }
 
+        /// <summary>
+        /// 获取学校的详细信息(用于登录或其他)
+        /// </summary>
+        /// <param name="school">School Object</param>
+        /// <param name="secretKey">SecretKey Object</param>
+        /// <returns>SchoolDetails</returns>
         public Task<SchoolDetails> GetSchoolDetailsAsync(School school, SecretKey secretKey)
         {
             return GetSchoolDetailsAsync(school.Id, secretKey.Chk);
@@ -241,7 +273,13 @@ namespace cpdaily_auto_submit
 
         }
 
-        public async Task MobileLogin(string phoneNumber, string chk)
+        /// <summary>
+        /// 手机验证码登录(发送验证码)
+        /// </summary>
+        /// <param name="phoneNumber">手机号</param>
+        /// <param name="chk">通过GetSecretKeyAsync()获取到的Chk密钥</param>
+        /// <returns>void</returns>
+        public async Task MobileLoginAsync(string phoneNumber, string chk)
         {
             string mobile = CpdailyCrypto.DESEncrypt(phoneNumber, CpdailyCrypto.GetDESKey(chk), CpdailyCrypto.IV);
             string url = "https://mobile.campushoy.com/app/auth/authentication/mobile/messageCode/v-8213";
@@ -265,7 +303,14 @@ namespace cpdaily_auto_submit
             // TODO: parse error message.
         }
 
-        public async Task<LoginResult> MobileLogin(string phoneNumber, string code, string chk)
+        /// <summary>
+        /// 手机验证码登录(提交验证码)
+        /// </summary>
+        /// <param name="phoneNumber">手机号码</param>
+        /// <param name="code">验证码</param>
+        /// <param name="chk">通过GetSecretKeyAsync()获取到的Chk密钥</param>
+        /// <returns>LoginResult</returns>
+        public async Task<LoginResult> MobileLoginAsync(string phoneNumber, string code, string chk)
         {
             string data = CpdailyCrypto.DESEncrypt(
                 JsonConvert.SerializeObject(new { d = code, c = phoneNumber }),
@@ -298,7 +343,12 @@ namespace cpdaily_auto_submit
             return json.ToObject<LoginResult>();
         }
 
-        public async Task UserStoreAppList(LoginResult loginResult)
+        /// <summary>
+        /// 获取用于访问校内应用的 Cookie
+        /// </summary>
+        /// <param name="loginResult">LoginResult Object</param>
+        /// <returns>Cookie string</returns>
+        public async Task<string> UserStoreAppListAsync(LoginResult loginResult)
         {
             string sessionToken = CpdailyCrypto.DESEncrypt(
                 loginResult.SessionToken,
@@ -330,10 +380,9 @@ namespace cpdaily_auto_submit
                 "XCE927==",
                 CpdailyCrypto.IV);
 
-
-
             string url = $"https://cqjtu.campusphere.net/wec-portal-mobile/client/userStoreAppList?oick={CpdailyCrypto.GetOick(loginResult.UserId)}";
             IRestResponse response = null;
+            // RestSharp 不会处理复杂的跳转，因此自行循环处理
             do
             {
                 RestClient client = new RestClient(url)
@@ -357,13 +406,19 @@ namespace cpdaily_auto_submit
 
                 url = response.Headers.Where(x => x.Name == "Location").Select(x => x.Value.ToString()).FirstOrDefault();
             } while (!string.IsNullOrEmpty(url));
-
+            StringBuilder sb = new StringBuilder();
             foreach (var cookie in response.Cookies)
             {
-                Console.WriteLine($"{cookie.Name}:{cookie.Value};");
+                sb.Append($"{cookie.Name}:{cookie.Value}; ");
             }
+            return sb.ToString();
         }
 
+        /// <summary>
+        /// 获取未完成的表单项目
+        /// </summary>
+        /// <param name="cookies">用于访问校内应用的 Cookie</param>
+        /// <returns>FormItem[]</returns>
         public async Task<FormItem[]> GetFormItemsAsync(string cookies)
         {
             string url = "https://cqjtu.campusphere.net/wec-counselor-collector-apps/stu/collector/queryCollectorProcessingList";
@@ -381,6 +436,13 @@ namespace cpdaily_auto_submit
             return list.ToObject<FormItem[]>();
         }
 
+        /// <summary>
+        /// 获取表单的字段
+        /// </summary>
+        /// <param name="cookies">用于访问校内应用的 Cookie</param>
+        /// <param name="wid">FormItem.Wid</param>
+        /// <param name="formWid">FormItem.FormWid</param>
+        /// <returns>FormField[]</returns>
         public async Task<FormField[]> GetFormFieldsAsync(string cookies, string wid, string formWid)
         {
             string url = "https://cqjtu.campusphere.net/wec-counselor-collector-apps/stu/collector/getFormFields";
