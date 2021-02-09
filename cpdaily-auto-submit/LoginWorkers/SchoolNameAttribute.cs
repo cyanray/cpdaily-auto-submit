d485f66dd3f53f0d5c6f457eaeb6eeab6ee75100
cyanray <a484599279@hotmail.com>
2021-02-10 03:30:38 +0800

    新增 SchoolNameAttribute.

---
+++ cpdaily-auto-submit/LoginWorkers/SchoolNameAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cpdaily_auto_submit.LoginWorkers
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class SchoolNameAttribute : Attribute
+    {
+        public string Name;
+        public SchoolNameAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
