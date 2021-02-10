1a64217c0be87853c0819cee1ad6e44588d55664
cyanray <a484599279@hotmail.com>
2021-02-10 17:42:49 +0800

    Draft AppConfig class.

---
+++ cpdaily-auto-submit/Models/AppConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cpdaily_auto_submit.Models
+{
+    public class AppConfig
+    {
+        public string Username { get; set; } = null;
+        public string Password { get; set; } = null;
+        public string SchoolName { get; set; } = null;
+    }
+}
