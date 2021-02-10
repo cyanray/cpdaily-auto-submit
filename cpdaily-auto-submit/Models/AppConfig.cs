7240e667a8a086d4a8b9d39022e8f3a95a714bf6
cyanray <a484599279@hotmail.com>
2021-02-10 22:30:17 +0800

    AppConfig: add FormFields item.

--- cpdaily-auto-submit/Models/AppConfig.cs
+++ cpdaily-auto-submit/Models/AppConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cpdaily_auto_submit.CpdailyModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,5 +9,6 @@ namespace cpdaily_auto_submit.Models
     {
         public string SchoolName { get; set; } = null;
         public List<User> Users { get; set; } = new List<User>();
+        public List<FormFieldChange> FormFields { get; set; } = new List<FormFieldChange>();
     }
 }
